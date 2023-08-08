b6402e7b045aa751a30e4397454f7d90da121db7
Ramani M <51358113+ramanicsegnxt@users.noreply.github.com>
2023-08-08 15:04:46 +0530

    Add files via upload
    G4B1 DSA Graded Coding Assignment 2

---
+++ G4B1_DSA_GradedCodingAssignment2/src/com/greatlearning/groupassignment/driver/SkyscraperDriver.java
@@ -0,0 +1,22 @@
+package com.greatlearning.groupassignment.driver;
+
+import com.greatlearning.groupassignment.question1.SkyscraperConstruction;
+
+import java.util.Scanner;
+
+public class SkyscraperDriver {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Question 1: Skyscraper Construction:");
+        // question 1: driver code to get order of construction
+        SkyscraperConstruction construction = new SkyscraperConstruction();
+
+        // call to get user input related to building details
+        construction.getFloorDetails(scanner);
+
+        // call to print order of construction based on the user input
+        construction.orderOfConstruction();
+        scanner.close();
+    }
+}
---
+++ G4B1_DSA_GradedCodingAssignment2/src/com/greatlearning/groupassignment/question2/Node.java
@@ -0,0 +1,10 @@
+package com.greatlearning.groupassignment.question2;
+
+public class Node {
+    int data;
+    public Node left, right;
+    public Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
